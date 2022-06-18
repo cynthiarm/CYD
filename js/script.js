816ea1ee9cf64d87554b700c1b73253a2c383d81
cynthiarm21 <cyntiff@gmail.com>
2022-06-17 19:34:49 -0700

    getaquote

--- js/script.js
+++ js/script.js
@@ -168,6 +168,62 @@
 			}
 		  });*/
 
+
+  /**
+   * Sticky header on scroll
+   */
+   const selectHeader = document.querySelector('#header');
+   if (selectHeader) {
+	 document.addEventListener('scroll', () => {
+	   window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+	 });
+   }
+ 
+   /**
+	* Navbar links active state on scroll
+	*/
+   let navbarlinks = document.querySelectorAll('#navbar .scrollto');
+ 
+   function navbarlinksActive() {
+	 navbarlinks.forEach(navbarlink => {
+ 
+	   if (!navbarlink.hash) return;
+ 
+	   let section = document.querySelector(navbarlink.hash);
+	   if (!section) return;
+ 
+	   let position = window.scrollY;
+	   if (navbarlink.hash != '#header') position += 200;
+ 
+	   if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+		 navbarlink.classList.add('active');
+	   } else {
+		 navbarlink.classList.remove('active');
+	   }
+	 })
+   }
+   window.addEventListener('load', navbarlinksActive);
+   document.addEventListener('scroll', navbarlinksActive);
+ 
+   /**
+	* Function to scroll to an element with top ofset
+	*/
+   function scrollto(el) {
+	 const selectHeader = document.querySelector('#header');
+	 let offset = 0;
+ 
+	 if (selectHeader.classList.contains('sticked')) {
+	   offset = document.querySelector('#header.sticked').offsetHeight;
+	 } else if (selectHeader.hasAttribute('data-scrollto-offset')) {
+	   offset = selectHeader.offsetHeight - parseInt(selectHeader.getAttribute('data-scrollto-offset'));
+	 }
+	 window.scrollTo({
+	   top: document.querySelector(el).offsetTop - offset,
+	   behavior: 'smooth'
+	 });
+   }
+
+
 		  // banner-carousel
 		function bannerCarouselOne() {
 			$('.banner-carousel.banner-carousel-1').slick({
